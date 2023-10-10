eb6c5a98003e4cf57ea3c054c1d9ab7bc4d88ab8
lawrencemonk1 <lawrencemonk.dev@gmail.com>
2023-10-10 21:05:34 +0200

    refactor: PASS_FMT_MESSAGE/FAIL_FMT_MESSAGE Macro for print formatted test message. Increased code readabilty.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,17 @@
-#include "ANSI-color-codes.h"
 #include <stdbool.h>
 #include <stdio.h>
+
+#define BBLK "\033[1;30m" // Bold Black
+#define REDB "\033[41m"   // Red BG
+#define GRNB "\033[42m"   // Green BG
+#define CRESET "\033[0m"  // Reset text to normal
+
+#define PASS_FMT_MSG(description)                                              \
+  printf(BBLK GRNB " PASS " CRESET " %s\n✓ %s\n\n", __FILE__, description)
+
+#define FAIL_FMT_MSG(description)                                              \
+  printf(BBLK REDB " FAIL " CRESET " %s\n✖ %s\n\n", __FILE__, description)
+
 // typedef struct test_r {
 //   bool pass;
 // } test_r;
@@ -12,12 +23,23 @@
 //   test_r result;     // test result
 // };
 //
+// Maybe we should store a test_state type?
+//
+// typedef enum State {
+//  RUNNING, // When implementing multi-thread
+//  PASS,
+//  FAIL
+// } State
+//
+// // Maybe a union? the test could be in only one state at a time.
+// typedef struct test_state {
+//    State state;
+// } test_state
+
 int sum(int a, int b) { return a + b; }
 
 bool test(char *description, int expr, int expected) {
-  expr == expected
-      ? printf(BBLK GRNB " PASS " reset " %s\n✓ %s\n\n", __FILE__, description)
-      : printf(BBLK REDB " FAIL " reset " %s\n✖ %s\n\n", __FILE__, description);
+  expr == expected ? PASS_FMT_MSG(description) : FAIL_FMT_MSG(description);
   return expr == expected;
 }
 
